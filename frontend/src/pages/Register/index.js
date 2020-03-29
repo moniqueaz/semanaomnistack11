fe831e213e7b1685695b670ad4a5a47b99d7c1a0
Monique Azevedo <monique.silva@maxmilhas.com.br>
2020-03-29 13:11:15 -0300

    Corrige testo em register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -13,7 +13,7 @@ const Register = () => {
         <section>
           <img src={logoImg} alt="Be The Hero"/>
 
-          <h1>Cadatro</h1>
+          <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
 
           <Link className="back-link"to="/">
